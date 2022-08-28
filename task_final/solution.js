364ea0446b823e64952e391e5b5c643b65f30b80
loki1302 <sir.bu4atsky@yandex.ru>
2022-08-28 23:43:38 +0400

    task_final DONE

--- task_final/solution.js
+++ task_final/solution.js
@@ -32,22 +32,24 @@
 //      ]
 //    }
 // }
-
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    let data = {client: '', order: {}, goods: []};
+    
+    data.client = `${name} ${phone}`;
+    
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    
+    data.order.sum = sum;
+    
+    for (let i = 0; i < goods.length; i++) {
+    
+    data.goods.push({title: goods[i].title, count: goods[i].count});
+    
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    
+    let jsonData = JSON.stringify({data: data});
+    
     return jsonData;
-}
+    
+    }
